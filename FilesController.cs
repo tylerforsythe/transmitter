9a690ee6cb7adc957e3f9ad1a9df34ff1971905e
Tyler Forsythe <forsythe.tyler@gmail.com>
2020-08-14 09:11:37 -0600

    Initial commit of OWIN code and a comment in FilesController about how to proceed next.

---
+++ FilesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Transmitter
+{
+    /// <summary>
+    /// Create methods for:
+    /// Receiving a file in chunks: file name, chunk, chunk size, SHA2
+    /// Validate a file: file name, size in bytes, SHA2
+    /// (WISH LIST) Transmit a file: file name, SHA2, destination address
+    /// </summary>
+    public class FilesController : ApiController 
+    { 
+        // GET api/values 
+        public IEnumerable<string> Get() {
+            Console.WriteLine("Files GET");
+            return new string[] { "value1", "value2" }; 
+        } 
+
+        // GET api/values/5 
+        public string Get(int id) 
+        { 
+            return "value"; 
+        } 
+
+        // POST api/values 
+        public void Post([FromBody]string value) 
+        { 
+        } 
+
+        // PUT api/values/5 
+        public void Put([FromBody]string value) 
+        {
+            Console.WriteLine($"Files PUT {value}");
+        } 
+
+        // DELETE api/values/5 
+        public void Delete(int id) 
+        { 
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace Transmitter
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string baseAddress = "http://localhost:9000/"; 
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress)) {
+                FetchTest(baseAddress);
+
+                Console.WriteLine("Waiting for key to exit...");
+
+                Console.ReadLine();
+            } 
+
+        }
+
+        private static void FetchTest(string baseAddress) {
+            // Create HttpClient and make a request to api/values 
+            HttpClient client = new HttpClient();
+
+            var response = client.GetAsync(baseAddress + "api/values").Result;
+
+            Console.WriteLine(response);
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
